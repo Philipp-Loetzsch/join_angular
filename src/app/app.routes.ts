8b2daa33172e8f6ad00b486e8c91b24b98761cfb
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-12-18 12:26:57 +0100

    define filestructure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
+import { LogInComponent } from './components/landing-page/log-in/log-in.component';
+import { SignInComponent } from './components/landing-page/sign-in/sign-in.component';
 import { MainComponent } from './components/main/main.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { AddTaskComponent } from './components/add-task/add-task.component';
-import { BoardComponent } from './components/board/board.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { HelpComponent } from './components/help/help.component';
-import { LegalNoticeComponent } from './components/legal-notice/legal-notice.component';
-import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { SummaryComponent } from './components/main/summary/summary.component';
+import { AddTaskComponent } from './components/main/add-task/add-task.component';
+import { BoardComponent } from './components/main/board/board.component';
+import { ContactsComponent } from './components/main/contacts/contacts.component';
+import { HelpComponent } from './components/main/help/help.component';
+import { LegalNoticeComponent } from './components/main/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './components/main/privacy-policy/privacy-policy.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: LogInComponent },
-  { path: 'sign_in', component: SignInComponent },
+  { path: '', component: LandingPageComponent, 
+    children: [
+      { path: '', component: LogInComponent },
+      { path: 'sign_in', component: SignInComponent },
+    ]
+  },
+
   { path: 'join', component: MainComponent,
     children: [
         {path:'summary', component: SummaryComponent},
