d44f00031dcff54e10dd0e7a19ba6b6995bd78f7
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-12-18 21:23:35 +0100

    style log in v0.1

--- src/app/components/landing-page/log-in/log-in.component.ts
+++ src/app/components/landing-page/log-in/log-in.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+  loginForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
 
+  onSubmit(){
+    
+  }
 }
