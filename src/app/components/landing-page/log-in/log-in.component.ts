5caa0208cb94bd1b9f4da4aeadef66bf5665d228
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-12-19 13:44:46 +0100

    style login v0.2

--- src/app/components/landing-page/log-in/log-in.component.ts
+++ src/app/components/landing-page/log-in/log-in.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
   imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.scss'
+  styleUrl: './log-in.component.scss',
 })
 export class LogInComponent {
   loginForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
-  onSubmit(){
-    
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      // Markiere alle Felder als "touched", um die Fehlermeldungen anzuzeigen
+      this.loginForm.markAllAsTouched();
+      return; // Verhindere das Absenden des Formulars
+    }
+    console.log('hat geklappt');
+
+    console.log(this.loginForm.value.email);
   }
+
 }
