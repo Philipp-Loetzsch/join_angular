29dd8a57bf00574a46a5deb550bfed11722a888a
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-12-20 14:45:35 +0100

    create navbar main

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
+interface Links {
+  name: string;
+  img: string;
+}
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatSidenavModule, CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
-
+  nameLinks: Array<Links> = [
+    { name: 'Summary', img: 'summary' },
+    { name: 'Add Task', img: 'add_task' },
+    { name: 'Board', img: 'board' },
+    { name: 'Contacts', img: 'contacts' },
+  ];
 }
