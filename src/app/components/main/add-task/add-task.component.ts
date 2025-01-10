35111bde8f4589e4f2d1de5adfe75867d10372e3
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-01-10 21:19:42 +0100

    style add task

--- src/app/components/main/add-task/add-task.component.ts
+++ src/app/components/main/add-task/add-task.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
-
+ required: boolean = false
 }
