4c9c6ca3f30fcfa94cb868e8be04daf5128a52cb
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2025-01-13 15:27:16 +0100

    style contacts

--- src/app/components/main/contacts/contacts.component.ts
+++ src/app/components/main/contacts/contacts.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact, UserDatasService } from '../../../services/user-datas.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  contactsList!:Contact[]
+  lastLetter!:string
+  constructor(private userDataService: UserDatasService){
+  }
+  async ngOnInit(): Promise<void> {
+   this.contactsList = await this.userDataService.getUserContacts()
+   console.log(this.contactsList);
+   
+  }
+
+  getShortcut(name: string): string {
+    const parts = name.split(' ');
+    if (parts.length === 0) {
+      return ''; 
+    }
+    const firstPart = parts[0];   
+    const lastPart = parts[parts.length - 1]; 
+    const initials = firstPart.charAt(0) + lastPart.charAt(0);
+  
+    return initials.toUpperCase();
+  }
+
+  getFirstLetter(name:string){
+    const firstLetterName = name.charAt(0)
+    if (firstLetterName === this.lastLetter) return 
+    else{
+      this.lastLetter = firstLetterName
+      return firstLetterName
+    }
+  }
 
 }
--- src/app/services/user-datas.service.ts
+++ src/app/services/user-datas.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  getDocs,
+} from '@angular/fire/firestore';
 
+export interface Contact{
+  id:string,
+  name:string,
+  email:string,
+  phone:string,
+  color:string
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserDatasService {
+  private firestore = inject(Firestore);
+  constructor() {
+  }
 
-  constructor() { }
+  async getUserContacts() {
+    try {
+      const querySnapshot = await getDocs(collection(this.firestore, 'userDatas', '6mVBch0Q7YyYR1AAEnkR', 'contacts'));
+      const contacts = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.data()['name'],
+        email: doc.data()['email'],
+        phone: doc.data()['phone'],
+        color: doc.data()['color']
+      }));
+      contacts.sort((a, b) => a.name.localeCompare(b.name));
+      return contacts;
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Kontakte:', error);
+      throw error;
+    }
+  }
+  
+
 }
