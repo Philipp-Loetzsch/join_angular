de33bfc9b4805a0f2dc0ef2aa3bc51242e7a7429
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2025-01-13 12:21:03 +0100

    install firebase database

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,26 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'join-abfb6',
+        appId: '1:441645167066:web:8e4bd8f6847137a70a90e3',
+        storageBucket: 'join-abfb6.firebasestorage.app',
+        apiKey: 'AIzaSyCWSLaLJu1bstX_gXvxJC28e1ObxnuGNJY',
+        authDomain: 'join-abfb6.firebaseapp.com',
+        messagingSenderId: '441645167066',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
